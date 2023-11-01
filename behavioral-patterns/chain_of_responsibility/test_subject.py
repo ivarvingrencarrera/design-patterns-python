80a04cad1c99283230de7da8f9ee1a0d0ac4fd56
ivarvingrencarrera <ivar.carrera@outlook.com>
2023-11-01 15:20:05 -0300

    feat: Create Chain of Responsibility pattern

---
+++ behavioral-patterns/chain_of_responsibility/test_subject.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+
+from .subject import Subject
+
+
+class TestSubject(TestCase):
+    def create_subject(self, **kwargs: object) -> Subject:
+        subject = {
+            'subject_id': 54572,
+            'subject_name': 'Empreendedorismo e Negócios',
+            'subject_is_hybrid': False,
+            'shift_id': 5,
+            'shift_name': 'Virtual',
+            'offer_id': 4510104,
+            'offer_year': 2023,
+            'offer_semester': 2,
+            'offer_term': 1,
+            'offer_status': 'active',
+            'offer_group': 1,
+            'offer_team': 4,
+            'course_id': 2412,
+            'lms_course_id': 'SGA_53255_2412_2023_2_4510104',
+            'synced_at_lms': True,
+        } | kwargs
+        return Subject(**subject)
+
+    def test_semipresencial(self) -> None:
+        subject = self.create_subject(subject_is_hybrid=True)
+        self.assertEqual(subject.segment, 'Disciplina Semipresencial')
+
+    def test_assincrona(self) -> None:
+        subject = self.create_subject(course_id=128)
+        self.assertEqual(subject.segment, 'Disciplina Assíncrona da Graduação Presencial')
+
+    def test_microfundamento(self) -> None:
+        subject = self.create_subject(subject_name='Microfundamento: Empreendedorismo e Negócios')
+        self.assertEqual(subject.segment, 'Microfundamento')
+
+    def test_projeto(self) -> None:
+        subject = self.create_subject(subject_name='Projeto: Empreendedorismo e Negócios')
+        self.assertEqual(subject.segment, 'Projeto')
+
+    def test_grupos_de_estudos(self) -> None:
+        subject = self.create_subject(subject_name='Grupos de Estudos: Empreendedorismo e Negócios')
+        self.assertEqual(subject.segment, 'Grupos de Estudos')
+
+    def test_competencias_comportamentais(self) -> None:
+        subject = self.create_subject(
+            subject_name='Competências Comportamentais: Empreendedorismo e Negócios'
+        )
+        self.assertEqual(subject.segment, 'Competências Comportamentais')
+
+    def test_desafios_contemporaneos(self) -> None:
+        subject = self.create_subject(
+            subject_name='Desafios Contemporâneos: Empreendedorismo e Negócios'
+        )
+        self.assertEqual(subject.segment, 'Desafios Contemporâneos')
+
+    def test_bacharelado_licenciatura(self) -> None:
+        subject = self.create_subject()
+        self.assertEqual(subject.segment, 'Bacharelado | Licenciatura')
